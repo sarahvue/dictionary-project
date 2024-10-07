1194e899a4c7a605e66994e49431a731a37faa76
sarahvue <sarahvue0924@gmail.com>
2024-10-06 17:02:07 -0700

    Basic form behavior

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import "./App.css";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <div className="container">
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center appFooter">
+          Coded by Sarah. Open sourced{" "}
+          <a href="https://github.com/sarahvue/dictionary-project">here</a> and
+          hosted on{" "}
+          <a href="https://smv-dictionary.netlify.app/" target="blank">
+            Netlify.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
